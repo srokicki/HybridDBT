3047fccb3455c4c3bfbfd3c318ceebd5fbb6f583
Simon Rokicki <simon.rokicki@irisa.fr>
2016-11-24 13:39:43 +0100

    Adding start address for destination binaries and 128 bits word for bytecode and vliw binaries

--- includes/transformation/irScheduler.h
+++ includes/transformation/irScheduler.h
@@ -4,7 +4,7 @@
 #ifndef __IR_SCHEDULER
 #define __IR_SCHEDULER
 
-int scheduling(uint1 optLevel, uint8 basicBlockSize, uint32 bytecode[1024], uint32 binaries[1024],
+int scheduling(uint1 optLevel, uint8 basicBlockSize, uint128 bytecode[256], uint128 binaries[1024], uint16 addressInBinaries,
 		uint6 placeOfRegisters[512], uint6 numberFreeRegister, uint6 freeRegisters[64], uint4 issue_width,
 		uintIW way_specialisation, uint32 placeOfInstr[256]);
 
