028fc6508ea9a145c1d1ff8cacba8a6e9b027f9f
Simon Rokicki <simon.rokicki@irisa.fr>
2016-11-24 13:42:13 +0100

    Adding the definition of new types

--- includes/types.h
+++ includes/types.h
@@ -6,27 +6,13 @@
 
 #ifdef __USE_AC
 #include <lib/ac_int.h>
-
-typedef ac_int<128, false> uint128;
-typedef ac_int<128, true> int128;
-
-typedef ac_int<32, false> uint32;
-typedef ac_int<32, true> int32;
-
-
-typedef ac_int<16, false> uint16;
-typedef ac_int<16, true> int16;
-
-
-
-typedef ac_int<8, false> uint8;
-typedef ac_int<6, false> uint6;
-typedef ac_int<4, false> uint4;
-typedef ac_int<3, false> uint3;
-typedef ac_int<1, false> uint1;
-
+using namespace ac_intN;
 
 typedef ac_int<MAX_ISSUE_WIDTH * 2, false> uintIW;
+typedef ac_int<128, false> uint128;
+typedef ac_int<64, false> uint64;
+
+
 #endif
 
 
