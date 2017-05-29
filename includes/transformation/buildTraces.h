960557b0ad9bee59412608d3f3e87d88852c3bf3
Simon Rokicki <simon.rokicki@irisa.fr>
2017-05-29 11:32:30 +0200

    New transformation: build trace
    NOTE: this transformation is still under development
    This transformation will merge the different block of a same procedure
    in order to generate bigger blocks. Indeed, the current weakness of the
    dbt process is the way control structure are handled: in code with many
    small if then else, the VLIW will not be exploited at all.
    The current implementation is able to merge blocks of simple if then
    structures or to merge the resulting block with the next one...
    This code is not tested yet...

---
+++ includes/transformation/buildTraces.h
@@ -0,0 +1,15 @@
+/*
+ * buildTraces.h
+ *
+ *  Created on: 22 mai 2017
+ *      Author: simon
+ */
+
+#ifndef INCLUDES_TRANSFORMATION_BUILDTRACES_H_
+#define INCLUDES_TRANSFORMATION_BUILDTRACES_H_
+
+
+void buildTraces(DBTPlateform *platform, IRProcedure *procedure);
+
+
+#endif /* INCLUDES_TRANSFORMATION_BUILDTRACES_H_ */
