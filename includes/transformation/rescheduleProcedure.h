893a66c2a71586a3677e2d163ab5ac7c5b90ee4f
Simon Rokicki <simon.rokicki@irisa.fr>
2017-08-03 16:30:32 +0200

    Splitted reschedule procedure into two parts
    The first part do the scheduling and the second one commit the result.
    It allow an external transformation to trigger a rescheduling and to
    check if the new version is effectively better than the previous one. It
    is used this way for VLIW reconfiguration.

--- includes/transformation/rescheduleProcedure.h
+++ includes/transformation/rescheduleProcedure.h
@@ -11,5 +11,8 @@
 
 int rescheduleProcedure(DBTPlateform *platform, IRProcedure *procedure,int writePlace);
 
+IRProcedure* rescheduleProcedure_schedule(DBTPlateform *platform, IRProcedure *procedure,int writePlace);
+int rescheduleProcedure_commit(DBTPlateform *platform, IRProcedure *procedure,int writePlace, IRProcedure *schedulePlaces);
+
 
 #endif /* INCLUDES_TRANSFORMATION_RESCHEDULEPROCEDURE_H_ */
