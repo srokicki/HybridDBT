6cd07f25dc466915e6b91036c4c09c9be93aebcf
Simon Rokicki <simon.rokicki@irisa.fr>
2017-08-03 16:29:10 +0200

    Splitted reschedule procedure into two parts.

--- src/transformation/rescheduleProcedure.cpp
+++ src/transformation/rescheduleProcedure.cpp
@@ -10,6 +10,8 @@
 #include <transformation/irScheduler.h>
 #include <lib/endianness.h>
 #include <transformation/reconfigureVLIW.h>
+#include <transformation/rescheduleProcedure.h>
+
 
 /******************************************************************************************
  ******************************  Reschedule Procedure
@@ -33,13 +35,194 @@
 
 int rescheduleProcedure(DBTPlateform *platform, IRProcedure *procedure,int writePlace){
 
-	procedure->configuration = platform->vliwInitialConfiguration;
+	IRProcedure *scheduledProc = rescheduleProcedure_schedule(platform, procedure, writePlace);
+	return rescheduleProcedure_commit(platform, procedure, writePlace, scheduledProc);
+
+
+//	char issueWidth = getIssueWidth(procedure->configuration);
+//	char incrementInBinaries = (getIssueWidth(procedure->configuration)>4) ? 2 : 1;
+//	int originalWritePlace = writePlace;
+//	int *oldBlockStarts = (int*) malloc(procedure->nbBlock * sizeof(int));
+//
+//
+//	/******************************************************************************************
+//	 ******************************  Scheduling of all blocks
+//	 ******************************************************************************************
+//	 * For each block of the procedure, we will do the instruction scheduling and keep track of their
+//	 * previous start address as well as the place where the jump instruction (if any) as been scheduled.
+//	 ******************************************************************************************/
+//
+//	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
+//		IRBlock *block = procedure->blocks[oneBlock];
+//
+//		//We save the old block start address
+//		oldBlockStarts[oneBlock] = block->vliwStartAddress;
+//
+//		//We move instructions into bytecode memory
+//		for (int oneBytecodeInstr = 0; oneBytecodeInstr<block->nbInstr; oneBytecodeInstr++){
+//			writeInt(platform->bytecode, 16*oneBytecodeInstr + 0, block->instructions[4*oneBytecodeInstr + 0]);
+//			writeInt(platform->bytecode, 16*oneBytecodeInstr + 4, block->instructions[4*oneBytecodeInstr + 1]);
+//			writeInt(platform->bytecode, 16*oneBytecodeInstr + 8, block->instructions[4*oneBytecodeInstr + 2]);
+//			writeInt(platform->bytecode, 16*oneBytecodeInstr + 12, block->instructions[4*oneBytecodeInstr + 3]);
+//		}
+//
+//		//We initialize other memories
+//		for (int oneFreeRegister = 34; oneFreeRegister<63; oneFreeRegister++)
+//			platform->freeRegisters[oneFreeRegister-34] = oneFreeRegister;
+//
+//		for (int onePlaceOfRegister = 0; onePlaceOfRegister<64; onePlaceOfRegister++)
+//			platform->placeOfRegisters[256+onePlaceOfRegister] = onePlaceOfRegister;
+//
+//		//This is only for debug
+//		if (platform->debugLevel > 1)
+//			for (int i=0; i<block->nbInstr; i++)
+//				printBytecodeInstruction(i, readInt(platform->bytecode, i*16+0), readInt(platform->bytecode, i*16+4), readInt(platform->bytecode, i*16+8), readInt(platform->bytecode, i*16+12));
+//
+//
+//
+//		//We call the register
+//		int binaSize = irScheduler(platform, 1,block->nbInstr, writePlace, 29, procedure->configuration);
+//		if (block->jumpID == -1){
+//			writeInt(platform->vliwBinaries, 16*(writePlace+binaSize), 0);
+//			writeInt(platform->vliwBinaries, 16*(writePlace+binaSize)+4, 0);
+//			writeInt(platform->vliwBinaries, 16*(writePlace+binaSize)+8, 0);
+//			writeInt(platform->vliwBinaries, 16*(writePlace+binaSize)+12, 0);
+//
+//			if (issueWidth>4){
+//				writeInt(platform->vliwBinaries, 16*(writePlace + binaSize+1), 0);
+//				writeInt(platform->vliwBinaries, 16*(writePlace + binaSize+1)+4, 0);
+//				writeInt(platform->vliwBinaries, 16*(writePlace + binaSize+1)+8, 0);
+//				writeInt(platform->vliwBinaries, 16*(writePlace + binaSize+1)+12, 0);
+//
+//			}
+//
+//			binaSize += incrementInBinaries;
+//		}
+//
+//		block->vliwEndAddress = writePlace + binaSize;
+//		block->vliwStartAddress = writePlace;
+//
+//		if (block->jumpID != -1){
+//			int addressOfScheduledJump = platform->placeOfInstr[block->jumpID];
+//			block->jumpPlace = addressOfScheduledJump;
+//		}
+//
+//		writePlace+=binaSize;
+//
+//	}
+//
+//
+//	/******************************************************************************************
+//	 ******************************  Jump correction
+//	 ******************************************************************************************
+//	 * Now that all blocks have been scheduled correctly, we can go through all jump instruction and
+//	 * modify their destination to fit with the new block position.
+//	 ******************************************************************************************/
+//
+//	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
+//		IRBlock *block = procedure->blocks[oneBlock];
+//
+//		if (block->nbSucc>1){
+//			//Conditional block (br)
+//			int offset = 4*(block->successor1->vliwStartAddress - block->jumpPlace);
+//			unsigned int oldJump = readInt(platform->vliwBinaries, 16*block->jumpPlace);
+//			writeInt(platform->vliwBinaries, 16*block->jumpPlace, (oldJump & 0xfc00007f) | ((offset & 0x7ffff) << 7));
+//
+//
+//		}
+//		else if (block->jumpID != -1 && block->nbSucc == 1){
+//			int dest = 4*block->successor1->vliwStartAddress;
+//			unsigned int oldJump = readInt(platform->vliwBinaries, 16*block->jumpPlace);
+//			writeInt(platform->vliwBinaries, 16*block->jumpPlace, (oldJump & 0xfc00007f) | ((dest & 0x7ffff) << 7));
+//		}
+//	}
+//
+//	/******************************************************************************************
+//	 ******************************  Link to the newly generated code
+//	 ******************************************************************************************
+//	 * For each basic block in the procedure, we will insert jump instructions at the previous start
+//	 * address targeting to the new start address. This way, the execution will simply switch toward the newly
+//	 * generated binaries.
+//	 ******************************************************************************************/
+//
+//	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
+//		IRBlock *block = procedure->blocks[oneBlock];
+//		int originalEntry = oldBlockStarts[oneBlock];
+//
+//		if (getIssueWidth(platform->vliwInitialConfiguration) <= 4){
+//			writeInt(platform->vliwBinaries, 16*originalEntry+0, assembleIInstruction(VEX_GOTO, block->vliwStartAddress*4, 0));
+//			writeInt(platform->vliwBinaries, 16*originalEntry+4, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+8, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+12, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+16, getReconfigurationInstruction(procedure->configuration));
+//			writeInt(platform->vliwBinaries, 16*originalEntry+20, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+24, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+28, 0);
+//		}
+//		else{
+//			writeInt(platform->vliwBinaries, 16*originalEntry+0, assembleIInstruction(VEX_GOTO, block->vliwStartAddress*4, 0));
+//			writeInt(platform->vliwBinaries, 16*originalEntry+4, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+8, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+12, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+16, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+20, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+24, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+28, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+0, getReconfigurationInstruction(procedure->configuration));
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+4, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+8, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+12, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+16, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+20, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+24, 0);
+//			writeInt(platform->vliwBinaries, 16*originalEntry+32+28, 0);
+//		}
+//
+//		if (block->nbSucc == 0){
+//			writeInt(platform->vliwBinaries, 16*block->vliwEndAddress-16*incrementInBinaries, getReconfigurationInstruction(platform->vliwInitialConfiguration));
+//		}
+//
+//	}
+//
+//	if (platform->debugLevel > 1){
+//
+//		//This is only for debug
+//		for (int i=originalWritePlace;i<writePlace;i++){
+//			fprintf(stderr, "%d ", i);
+//			std::cerr << printDecodedInstr(platform->vliwBinaries[i].slc<32>(0)); fprintf(stderr, " ");
+//			std::cerr << printDecodedInstr(platform->vliwBinaries[i].slc<32>(32)); fprintf(stderr, " ");
+//			std::cerr << printDecodedInstr(platform->vliwBinaries[i].slc<32>(64)); fprintf(stderr, " ");
+//			std::cerr << printDecodedInstr(platform->vliwBinaries[i].slc<32>(96)); fprintf(stderr, " ");
+//
+//			if (issueWidth>4){
+//				std::cerr << printDecodedInstr(platform->vliwBinaries[i+1].slc<32>(0)); fprintf(stderr, " ");
+//				std::cerr << printDecodedInstr(platform->vliwBinaries[i+1].slc<32>(32)); fprintf(stderr, " ");
+//				std::cerr << printDecodedInstr(platform->vliwBinaries[i+1].slc<32>(64)); fprintf(stderr, " ");
+//				std::cerr << printDecodedInstr(platform->vliwBinaries[i+1].slc<32>(96)); fprintf(stderr, " ");
+//				i++;
+//			}
+//			fprintf(stderr, "\n");
+//
+//		}
+//
+//	}
+//
+//	return writePlace;
+
+}
+
+IRProcedure* rescheduleProcedure_schedule(DBTPlateform *platform, IRProcedure *procedure,int writePlace){
+
 	char issueWidth = getIssueWidth(procedure->configuration);
 	char incrementInBinaries = (getIssueWidth(procedure->configuration)>4) ? 2 : 1;
-	int originalWritePlace = writePlace;
-	int originalEntry = procedure->entryBlock->vliwStartAddress;
-	int *oldBlockStarts = (int*) malloc(procedure->nbBlock * sizeof(int));
 
+	IRBlock **blocks = (IRBlock **) malloc(procedure->nbBlock * sizeof(IRBlock*));
+	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
+		blocks[oneBlock] = new IRBlock(-1,-1,-1);
+	}
+	IRProcedure *result = new IRProcedure(blocks[0], procedure->nbBlock);
+	result->blocks = blocks;
+	result->configuration = procedure->configuration;
 
 	/******************************************************************************************
 	 ******************************  Scheduling of all blocks
@@ -51,9 +234,6 @@ int rescheduleProcedure(DBTPlateform *platform, IRProcedure *procedure,int write
 	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
 		IRBlock *block = procedure->blocks[oneBlock];
 
-		//We save the old block start address
-		oldBlockStarts[oneBlock] = block->vliwStartAddress;
-
 		//We move instructions into bytecode memory
 		for (int oneBytecodeInstr = 0; oneBytecodeInstr<block->nbInstr; oneBytecodeInstr++){
 			writeInt(platform->bytecode, 16*oneBytecodeInstr + 0, block->instructions[4*oneBytecodeInstr + 0]);
@@ -63,8 +243,8 @@ int rescheduleProcedure(DBTPlateform *platform, IRProcedure *procedure,int write
 		}
 
 		//We initialize other memories
-		for (int oneFreeRegister = 33; oneFreeRegister<63; oneFreeRegister++)
-			platform->freeRegisters[oneFreeRegister-33] = oneFreeRegister;
+		for (int oneFreeRegister = 34; oneFreeRegister<63; oneFreeRegister++)
+			platform->freeRegisters[oneFreeRegister-34] = oneFreeRegister;
 
 		for (int onePlaceOfRegister = 0; onePlaceOfRegister<64; onePlaceOfRegister++)
 			platform->placeOfRegisters[256+onePlaceOfRegister] = onePlaceOfRegister;
@@ -95,17 +275,41 @@ int rescheduleProcedure(DBTPlateform *platform, IRProcedure *procedure,int write
 			binaSize += incrementInBinaries;
 		}
 
-		block->vliwEndAddress = writePlace + binaSize;
-		block->vliwStartAddress = writePlace;
+		result->blocks[oneBlock]->vliwStartAddress = writePlace;
+		result->blocks[oneBlock]->vliwEndAddress = writePlace + binaSize;
 
 		if (block->jumpID != -1){
 			int addressOfScheduledJump = platform->placeOfInstr[block->jumpID];
-			block->jumpPlace = addressOfScheduledJump;
+			result->blocks[oneBlock]->jumpPlace = addressOfScheduledJump;
 		}
 
 		writePlace+=binaSize;
 
 	}
+
+	return result;
+
+}
+
+int rescheduleProcedure_commit(DBTPlateform *platform, IRProcedure *procedure,int writePlace, IRProcedure *scheduledProc){
+
+	char issueWidth = getIssueWidth(procedure->configuration);
+	char incrementInBinaries = (getIssueWidth(procedure->configuration)>4) ? 2 : 1;
+	int *oldBlockStarts = (int*) malloc(procedure->nbBlock * sizeof(int));
+	int originalWritePlace = writePlace;
+
+	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
+		oldBlockStarts[oneBlock] = procedure->blocks[oneBlock]->vliwStartAddress;
+
+		procedure->blocks[oneBlock]->vliwStartAddress = scheduledProc->blocks[oneBlock]->vliwStartAddress;
+		procedure->blocks[oneBlock]->vliwEndAddress = scheduledProc->blocks[oneBlock]->vliwEndAddress;
+		procedure->blocks[oneBlock]->jumpPlace = scheduledProc->blocks[oneBlock]->jumpPlace;
+
+		if (procedure->blocks[oneBlock]->vliwEndAddress > writePlace)
+			writePlace = procedure->blocks[oneBlock]->vliwEndAddress;
+	}
+
+
 
 	/******************************************************************************************
 	 ******************************  Jump correction
@@ -143,14 +347,38 @@ int rescheduleProcedure(DBTPlateform *platform, IRProcedure *procedure,int write
 	for (int oneBlock = 0; oneBlock<procedure->nbBlock; oneBlock++){
 		IRBlock *block = procedure->blocks[oneBlock];
 		int originalEntry = oldBlockStarts[oneBlock];
-		writeInt(platform->vliwBinaries, 16*originalEntry+0, assembleIInstruction(VEX_GOTO, block->vliwStartAddress*4, 0));
-		writeInt(platform->vliwBinaries, 16*originalEntry+4, 0);
-		writeInt(platform->vliwBinaries, 16*originalEntry+8, 0);
-		writeInt(platform->vliwBinaries, 16*originalEntry+12, 0);
-		writeInt(platform->vliwBinaries, 16*originalEntry+16, getReconfigurationInstruction(procedure->configuration));
-		writeInt(platform->vliwBinaries, 16*originalEntry+20, 0);
-		writeInt(platform->vliwBinaries, 16*originalEntry+24, 0);
-		writeInt(platform->vliwBinaries, 16*originalEntry+28, 0);
+
+		if (platform->vexSimulator->PC == originalEntry || platform->vexSimulator->PC == originalEntry+1)
+			platform->vexSimulator->doStep(2);
+
+		if (getIssueWidth(procedure->previousConfiguration) <= 4){
+			writeInt(platform->vliwBinaries, 16*originalEntry+0, assembleIInstruction(VEX_GOTO, block->vliwStartAddress*4, 0));
+			writeInt(platform->vliwBinaries, 16*originalEntry+4, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+8, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+12, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+16, getReconfigurationInstruction(procedure->configuration));
+			writeInt(platform->vliwBinaries, 16*originalEntry+20, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+24, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+28, 0);
+		}
+		else{
+			writeInt(platform->vliwBinaries, 16*originalEntry+0, assembleIInstruction(VEX_GOTO, block->vliwStartAddress*4, 0));
+			writeInt(platform->vliwBinaries, 16*originalEntry+4, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+8, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+12, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+16, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+20, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+24, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+28, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+0, getReconfigurationInstruction(procedure->configuration));
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+4, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+8, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+12, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+16, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+20, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+24, 0);
+			writeInt(platform->vliwBinaries, 16*originalEntry+32+28, 0);
+		}
 
 		if (block->nbSucc == 0){
 			writeInt(platform->vliwBinaries, 16*block->vliwEndAddress-16*incrementInBinaries, getReconfigurationInstruction(platform->vliwInitialConfiguration));
