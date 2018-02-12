d30410da6eec84a1510a2ad67f6ed8273f86744f
arthur blanleuil <ablanleu@tompouce2.irisa.fr>
2018-02-12 15:20:20 +0100

    Fix DFG printer

--- src/transformation/cgraScheduler.cpp
+++ src/transformation/cgraScheduler.cpp
@@ -1,8 +1,10 @@
 #include <transformation/cgraScheduler.h>
-
 #include <lib/log.h>
-
 #include <cstdio>
+
+#include <vector>
+#include <algorithm>
+
 
 void printGraph(uint128_struct * instructions, uint32_t numInstructions);
 
@@ -26,22 +28,34 @@ void CgraScheduler::schedule(CgraSimulator& cgra, uint128_struct * instructions,
 
 void printGraph(uint128_struct *instructions, uint32_t numInstructions)
 {
-	FILE * f = fopen("/home/ablanleu/Documents/stage/xdot/cgra.dot", "w");
+	static std::vector<uint128_struct*> blocs;
+
+	if (std::find(blocs.begin(), blocs.end(), instructions) != blocs.end())
+		return;
+
+	blocs.push_back(instructions);
+
+	FILE * f = fopen(std::string("/home/ablanleu/Documents/stage/xdot/cgra"+std::to_string(blocs.size())+".dot").c_str(), "w");
 	Log::fprintf(0, f, "digraph cgra {");
 	for (uint32_t i = 0; i < numInstructions; ++i)
 	{
-		Log::printf(0, "%s", printBytecodeInstruction(i, instructions[i].word96
-																									, instructions[i].word64
-																									, instructions[i].word32
-																									, instructions[i].word0).c_str());
-
+		uint32_t opCode = ((instructions[i].word96>>19) & 0x7f);
 		uint32_t typeCode = ((instructions[i].word96>>28) & 0x3);
 		uint32_t isImm = ((instructions[i].word96>>18) & 0x1);
 		uint16_t src1 = ((instructions[i].word96>>0) & 0x1ff);
 		uint16_t src2 = ((instructions[i].word64>>23) & 0x1ff);
+		uint16_t dst  = ((instructions[i].word64>>14) & 0x1ff);
+
+		Log::fprintf(0, f, "i%d [label=%s];", i, opcodeNames[opCode]);
 
 		if (typeCode == 0)
 		{
+			if (opCode == VEX_STD)
+				if (dst < 256)
+					Log::fprintf(0, f, "i%d -> i%d;", dst, i);
+				else
+					Log::fprintf(0, f, "r%d -> i%d;", dst-256, i);
+
 			if (src2 < 256)
 				Log::fprintf(0, f, "i%d -> i%d;", src2, i);
 			else
