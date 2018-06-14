18768c4615c2e0664112c84cf39d8e0f39a7dfdd
Simon Rokicki <simon.rokicki@irisa.fr>
2018-06-14 16:24:41 +0200

    Simulation of speculative load store with simple load store queue

--- includes/simulator/loadQueueVexSimulator.h
+++ includes/simulator/loadQueueVexSimulator.h
@@ -22,18 +22,24 @@ public:
 	ac_int<LOAD_QUEUE_ADDRESS_SIZE, false> loadQueue_addr[LOAD_QUEUE_NB_BANK][LOAD_QUEUE_BANK_SIZE];
 	ac_int<LOAD_QUEUE_AGE_SIZE, false> loadQueue_age[LOAD_QUEUE_NB_BANK][LOAD_QUEUE_BANK_SIZE];
 
+	//Functions that will be modified
+	void doDCMem(struct FtoDC ftoDC, struct DCtoEx *dctoEx);
+	void doMem(struct ExtoMem extoMem, struct MemtoWB *memtoWB);
+
 
 	//New functions will be added to handle speculation
-	ac_int<1, false> stbSpec(ac_int<64, false> addr, ac_int<8, true> value, unsigned char bankId);
-	ac_int<1, false> sthSpec(ac_int<64, false> addr, ac_int<16, true> value, unsigned char bankId);
-	ac_int<1, false> stwSpec(ac_int<64, false> addr, ac_int<32, true> value, unsigned char bankId);
-	ac_int<1, false> stdSpec(ac_int<64, false> addr, ac_int<64, true> value, unsigned char bankId);
+	ac_int<1, false> stbSpec(ac_int<64, false> addr, ac_int<8, true> value, unsigned char specId);
+	ac_int<1, false> sthSpec(ac_int<64, false> addr, ac_int<16, true> value, unsigned char specId);
+	ac_int<1, false> stwSpec(ac_int<64, false> addr, ac_int<32, true> value, unsigned char specId);
+	ac_int<1, false> stdSpec(ac_int<64, false> addr, ac_int<64, true> value, unsigned char specId);
 
-	ac_int<8, true> ldbSpec(ac_int<64, false> addr, unsigned char bankId);
-	ac_int<16, true> ldhSpec(ac_int<64, false> addr, unsigned char bankId);
-	ac_int<32, true> ldwSpec(ac_int<64, false> addr, unsigned char bankId);
-	ac_int<64, true> lddSpec(ac_int<64, false> addr, unsigned char bankId);
+	ac_int<8, true> ldbSpec(ac_int<64, false> addr, unsigned char specId);
+	ac_int<16, true> ldhSpec(ac_int<64, false> addr, unsigned char specId);
+	ac_int<32, true> ldwSpec(ac_int<64, false> addr, unsigned char specId);
+	ac_int<64, true> lddSpec(ac_int<64, false> addr, unsigned char specId);
 
+private:
+	ac_int<1, false> checkSpec(ac_int<64, false> addr, unsigned char specId);
 };
 
 #endif /* INCLUDES_SIMULATOR_LOADQUEUEVEXSIMULATOR_CPP_ */
--- src/simulator/loadQueueVexSimulator.cpp
+++ src/simulator/loadQueueVexSimulator.cpp
@@ -1,2 +1,142 @@
+#include <simulator/loadQueueVexSimulator.h>
+#include <isa/vexISA.h>
 
 
+ac_int<64, true> LoadQueueVexSimulator::lddSpec(ac_int<64, false> addr, unsigned char specId){
+
+	ac_int<3, false> bankId = bankId >> 2;
+	ac_int<2, false> indexInBank = specId & 0x3;
+
+	this->loadQueue_addr[bankId][indexInBank] = addr.slc<24>(3);
+	this->loadQueue_age[bankId][indexInBank] = 1;
+
+	return this->ldd(addr);
+}
+
+
+ac_int<1, false> LoadQueueVexSimulator::checkSpec(ac_int<64, false> addr, unsigned char specId){
+	ac_int<3, false> bankId = bankId >> 2;
+	ac_int<2, false> indexInBank = specId & 0x3;
+
+	for (int oneLoad = 0; oneLoad < LOAD_QUEUE_BANK_SIZE; oneLoad++){
+		if (this->loadQueue_age[bankId][oneLoad] && this->loadQueue_addr[bankId][indexInBank] == addr.slc<24>(3)){
+			fprintf(stderr, "Mispeculation !!\n");
+			exit(-1);
+		}
+	}
+}
+
+ac_int<1, false> LoadQueueVexSimulator::stbSpec(ac_int<64, false> addr, ac_int<8, true> value, unsigned char specId){
+	stb(addr, value);
+	return checkSpec(addr, specId);
+}
+ac_int<1, false> LoadQueueVexSimulator::sthSpec(ac_int<64, false> addr, ac_int<16, true> value, unsigned char specId){
+	sth(addr, value);
+	return checkSpec(addr, specId);
+}
+
+ac_int<1, false> LoadQueueVexSimulator::stwSpec(ac_int<64, false> addr, ac_int<32, true> value, unsigned char specId){
+	stw(addr, value);
+	return checkSpec(addr, specId);
+}
+
+ac_int<1, false> LoadQueueVexSimulator::stdSpec(ac_int<64, false> addr, ac_int<64, true> value, unsigned char specId){
+	std(addr, value);
+	return checkSpec(addr, specId);
+}
+
+
+void LoadQueueVexSimulator::doDCMem(struct FtoDC ftoDC, struct DCtoEx *dctoEx){
+	VexSimulator::doDCMem(ftoDC, dctoEx);
+
+	if ((dctoEx->opCode >> 3) == (VEX_LDD>>3) || dctoEx->opCode == VEX_FLW){
+		if (dctoEx->isSpec){
+			ac_int<64, false> address = dctoEx->dataa + dctoEx->datab;
+			dctoEx->memValue = lddSpec(address, (ftoDC.instruction>>8) & 0x1f);
+		}
+	}
+
+}
+
+void LoadQueueVexSimulator::doMem(ExtoMem extoMem, MemtoWB *memtoWB){
+	VexSimulator::doMem(extoMem, memtoWB);
+
+	if (!extoMem.isSpec)
+		return;
+
+	ac_int<64, false> address = extoMem.result;
+
+	if (extoMem.opCode == VEX_FSW || extoMem.opCode == VEX_FSH || extoMem.opCode == VEX_FSB){
+			memtoWB->WBena = 0; //TODO : this shouldn't be necessary : WB shouldn't be enabled before
+
+			if (extoMem.opCode == VEX_FSW){
+				unsigned int value;
+				memcpy(&value, &extoMem.floatRes, 4);
+
+				stwSpec(extoMem.result, value, extoMem.funct);
+			}
+			else if (extoMem.opCode == VEX_FSH){
+				unsigned int value;
+				memcpy(&value, &extoMem.floatRes, 2);
+				sthSpec(address, value, extoMem.funct);
+			}
+			else if (extoMem.opCode == VEX_FSW){
+				unsigned int value;
+				memcpy(&value, &extoMem.floatRes, 1);
+				stbSpec(address, value, extoMem.funct);
+			}
+		}
+		else if (extoMem.opCode.slc<3>(4) == 1){
+			//The instruction is a memory access
+
+			if (extoMem.opCode == VEX_PROFILE){
+
+				if (this->profileResult[extoMem.result] != 255)
+					this->profileResult[extoMem.result]++;
+
+				memtoWB->WBena = 0;
+			}
+			else {
+
+
+
+
+				memtoWB->WBena = 0; //TODO : this shouldn't be necessary : WB shouldn't be enabled before
+
+				//We are on a store instruction
+				ac_int<1, false> byteEna0=0, byteEna1=0, byteEna2=0, byteEna3=0, byteEna4=0, byteEna5=0, byteEna6=0, byteEna7=0;
+				ac_int<8, false> byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7;
+				ac_int<7, false> opcodeToSwitch = extoMem.opCode;
+				ac_int<1, false> enableStore = 1;
+
+				if (opcodeToSwitch > VEX_STD){
+					opcodeToSwitch -= 4;
+					enableStore = this->enable;
+				}
+
+				if (enableStore){
+					switch (extoMem.opCode){
+					case VEX_STD:
+						this->stdSpec(address, extoMem.datac, extoMem.funct);
+						break;
+					case VEX_STW:
+
+						this->stwSpec(address, extoMem.datac.slc<32>(0), extoMem.funct);
+
+					break;
+					case VEX_STH:
+						//STH
+
+						this->sthSpec(address, extoMem.datac.slc<16>(0), extoMem.funct);
+					break;
+					case VEX_STB:
+
+						this->stbSpec(address, extoMem.datac.slc<8>(0), extoMem.funct);
+
+						break;
+					default:
+					break;
+					}
+				}
+
+}
