e1367999c5b06b1eb92b39c5aa86ae5f8845eaf3
Simon Rokicki <simon.rokicki@irisa.fr>
2019-04-04 09:42:52 +0200

    Improving check speculation

--- src/transformation/checkSpeculation.cpp
+++ src/transformation/checkSpeculation.cpp
@@ -22,33 +22,44 @@ void checkSpeculation(IRProcedure* procedure){
 		for (unsigned i_br = 0; i_br < trace->nbInstr; i_br++){
 			char br_opcode = getOpcode(trace->instructions, i_br);
 
-			if (br_opcode >= VEX_GOTO && br_opcode <= VEX_BGEU){
-
+			if (br_opcode == VEX_BR || br_opcode == VEX_BRF || br_opcode == VEX_BLT || br_opcode == VEX_BGE || br_opcode == VEX_BLTU || br_opcode == VEX_BGEU ){
+				fprintf(stderr, "For br %d\n", i_br);
 
 				int taint[NB_REG] = { 0 };
 
-				for (unsigned i_instr = i_br + 1; i_instr < i_br + SEW_SIZE && i_instr < trace->nbInstr; i_instr ++){
+				for (unsigned i_instr = i_br + 1; i_instr < trace->nbInstr; i_instr ++){
+
 					char instr_opcode = getOpcode(trace->instructions, i_instr);
-					if (instr_opcode >= VEX_LDD && instr_opcode <= VEX_PROFILE){ // if instruction is a load after a branch
-						short instr_destreg = getDestinationRegister(trace->instructions, i_instr);
-
-						if (instr_destreg >= 0) //if we have a dest
-							taint[instr_destreg] = 1; //the dest is tainted
-					}
-
 					short operands[] = {0,0};
 					char nb_operands = getOperands(trace->instructions, i_instr, operands);
 
+					//We check if operands are tainted
 					for (unsigned oneOperand = 0; oneOperand < nb_operands; oneOperand++)
 					{
+						if (operands[oneOperand] < 256)
+							operands[oneOperand] = getDestinationRegister(trace->instructions, operands[oneOperand]);
+
 						if (taint[operands[oneOperand]]) //if operand is tainted
 						{
 							short instr_destreg = getDestinationRegister(trace->instructions, i_instr);
 							if (instr_destreg >= 0) //if there is a dest
 								taint[instr_destreg] = 1; //we taint the destination register
 
-							if (instr_opcode >= VEX_LDD && instr_opcode <= VEX_PROFILE) //if instr is a load (has an impact on something)
+							if (instr_opcode >= VEX_LDD && instr_opcode < VEX_PROFILE){ //if instr is a load (has an impact on something)
 								addControlDep(trace->instructions, i_br, i_instr); //we add a control dep between branch and intr
+								Log::printf(LOG_CHECK_SPECULATION, "Found a dep to add to %d\n", i_instr);
+							}
+						}
+					}
+
+					//We taint the destination
+					if (instr_opcode >= VEX_LDD && instr_opcode < VEX_PROFILE){ // if instruction is a load after a branch
+						short instr_destreg = getDestinationRegister(trace->instructions, i_instr);
+
+						fprintf(stderr, "Found a load\n");
+						if (instr_destreg >= 0){ //if we have a dest
+							taint[instr_destreg] = 1; //the dest is tainted
+							fprintf(stderr, "Tainting %d\n", instr_destreg);
 						}
 					}
 				}
